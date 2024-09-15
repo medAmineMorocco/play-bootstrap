596f63f33e8adf1bc690fe0edca1fcb0a2c61479
medamine <mohamedamineammach@gmail.com>
2024-09-15 23:28:59 +0100

    enhance design of footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -9,8 +9,8 @@ const Footer = () => {
       data-wow-delay=".15s"
     >
       <div className="container">
-        <div className="w-full -mx-4 flex flex-wrap">
-          <div className="w-full px-4 sm:w-1/2 md:w-1/2 lg:w-4/12 xl:w-3/12">
+        <div className="w-full">
+          <div className="w-full text-center">
             <div className="mb-10 w-full">
               <Link href="/" className="mb-6 inline-block max-w-[160px]">
                 <Image
@@ -21,10 +21,10 @@ const Footer = () => {
                   className="max-w-full"
                 />
               </Link>
-              <p className="mb-8 max-w-[270px] text-base text-gray-7">
+              <p className="mb-8 w-full text-base text-gray-7">
                 We streamline worktree management for software engineers and companies with WorktreeWise.
               </p>
-              <div className="-mx-3 flex items-center">
+              <div className="-mx-3 flex justify-center">
                 <a
                   aria-label="social link"
                   href="/#"
@@ -49,33 +49,27 @@ const Footer = () => {
               </div>
             </div>
           </div>
-          <div className="mt-12 border-t border-[#8890A4] border-opacity-40 py-8 lg:mt-[60px] flex-grow">
+          <div className="border-t border-[#8890A4] border-opacity-40 py-8 flex-grow">
             <div className="container">
-              <div className="-mx-4 flex flex-wrap justify-end">
-                <div className="w-full px-4 md:w-2/3 lg:w-1/2">
-                  <div className="my-1">
-                    <div className="-mx-3 flex items-center justify-center md:justify-start">
-                      <a
-                        href="/#"
-                        className="px-3 text-base text-gray-7 hover:text-white hover:underline"
-                      >
-                        Privacy policy
-                      </a>
-                      <a
-                        href="/#"
-                        className="px-3 text-base text-gray-7 hover:text-white hover:underline"
-                      >
-                        Legal notice
-                      </a>
-                      <a
-                        href="/#"
-                        className="px-3 text-base text-gray-7 hover:text-white hover:underline"
-                      >
-                        Terms of service
-                      </a>
-                    </div>
-                  </div>
-                </div>
+              <div className="flex justify-center">
+                <a
+                  href="/#"
+                  className="px-3 text-base text-gray-7 hover:text-white hover:underline"
+                >
+                  Privacy policy
+                </a>
+                <a
+                  href="/#"
+                  className="px-3 text-base text-gray-7 hover:text-white hover:underline"
+                >
+                  Legal notice
+                </a>
+                <a
+                  href="/#"
+                  className="px-3 text-base text-gray-7 hover:text-white hover:underline"
+                >
+                  Terms of service
+                </a>
               </div>
             </div>
           </div>
