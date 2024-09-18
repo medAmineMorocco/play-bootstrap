44e16d2da54433450017bc8d5a7577c7bf06e7ad
medamine <mohamedamineammach@gmail.com>
2024-09-18 20:26:53 +0100

    sticky bar: remove close btn

--- src/components/StickyBar/StickyBar.tsx
+++ src/components/StickyBar/StickyBar.tsx
@@ -11,31 +11,6 @@ export default function StickyBar({text}:{text: string}) {
               Get it now→
             </a>
           </p>
-
-          <button className="absolute right-0 top-1/2 -translate-y-1/2 text-dark-4 duration-200 hover:text-white">
-            <svg
-              width="14"
-              height="14"
-              viewBox="0 0 14 14"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g clip-path="url(#clip0_2386_1123)">
-                <path
-                  d="M1 1L7 7M7 7L13 13M7 7L13 1M7 7L1 13"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </g>
-              <defs>
-                <clipPath id="clip0_2386_1123">
-                  <rect width="14" height="14" fill="white" />
-                </clipPath>
-              </defs>
-            </svg>
-          </button>
         </div>
       </div>
     </section>
