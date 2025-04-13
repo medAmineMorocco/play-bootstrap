2b54497d414757114c2de85c8bab7c71d30eb926
medamine <mohamedamineammach@gmail.com>
2025-04-13 23:56:09 +0100

    conditional render older versions

--- src/components/Hero/Download.tsx
+++ src/components/Hero/Download.tsx
@@ -67,7 +67,7 @@ const Download = () => {
                     <DropdownItem newTab={false} label={<span><LinuxOutlined  style={{ fontSize: '24px'}}/> (.deb)</span>} href={process.env.NEXT_PUBLIC_DOWNLOAD_LINUX_DEB_URL || ""} />
                     <DropdownItem newTab={false} label={<span><LinuxOutlined  style={{ fontSize: '24px'}}/> (.rpm)</span>} href={process.env.NEXT_PUBLIC_DOWNLOAD_LINUX_RPM_URL || ""} />
                     <DropdownItem newTab={false} label={<span><LinuxOutlined  style={{ fontSize: '24px'}}/> (Snap)</span>} href={process.env.NEXT_PUBLIC_DOWNLOAD_LINUX_SNAP_URL || ""} />
-                    <DropdownItem newTab={true} label="Older versions" href={process.env.NEXT_PUBLIC_DOWNLOAD_OLDER_VERSION_URL || ""} />
+                    {process.env.NEXT_PUBLIC_DOWNLOAD_OLDER_VERSION_URL && <DropdownItem newTab={true} label="Older versions" href={process.env.NEXT_PUBLIC_DOWNLOAD_OLDER_VERSION_URL} />}
                   </div>
                 </div>
               </div>
