546e561ea993712ac9f0599a0ac7790fd580e26f
medamine <mohamedamineammach@gmail.com>
2025-10-29 23:17:02 +0100

    seo: fix page en double

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.worktreewise.com' }],
+        destination: 'https://worktreewise.com/:path*',
+        permanent: true,
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
--- src/app/head.tsx
+++ src/app/head.tsx
@@ -13,7 +13,7 @@ export default function Head() {
       <meta name="description"
             content="Effortlessly manage your Git worktrees with WorktreeWise. Including features to add and remove git worktrees, open them in your favorite editor, change their locations, and more. All these capabilities work together to enhance your productivity and streamline your development process." />
       <meta name="keywords"
-            content="git worktree, git diff, git worktree add, git-worktree, git worktree remove, git worktree example, git worktree tutorial, how to use git worktree, what is git worktree" />
+            content="worktreewise, git worktree, git diff, git worktree add, git-worktree, git worktree remove, git worktree example, git worktree tutorial, how to use git worktree, what is git worktree" />
       <link rel="icon" href="/favicon.ico" type="image/x-icon" />
       <link rel="canonical" href="https://worktreewise.com" />
       <link rel="preconnect" href="https://static.hotjar.com" />
