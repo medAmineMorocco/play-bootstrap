2a5d1cfbdbe9b16a7dbfa9073a583b112f37f498
medamine <mohamedamineammach@gmail.com>
2025-11-30 22:18:41 +0100

    fix blog styles

--- src/app/(site)/blog/page.tsx
+++ src/app/(site)/blog/page.tsx
@@ -45,7 +45,7 @@ const Blog = () => {
 
       <section className="pb-10 pt-20 lg:pb-20 lg:pt-[120px]">
         <div className="container">
-          <div className="-mx-4 flex flex-wrap justify-center">
+          <div className="-mx-4 flex flex-wrap">
             {posts.map((blog, i) => (
               <div key={i} className="w-full px-4 md:w-2/3 lg:w-1/2 xl:w-1/3">
                 <SingleBlog blog={blog} />
